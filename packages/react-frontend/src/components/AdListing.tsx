41858b6342f52804934302910fe1a7f42adcad75
= <=>
2024-02-22 19:53:24 +0200

    Added dots on the photos carousel

--- packages/react-frontend/src/components/AdListing.tsx
+++ packages/react-frontend/src/components/AdListing.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { type CarouselApi } from "@/components/ui/carousel";
 
 import { AdModel } from "@/types/adTypes";
 import { Card, CardContent, CardTitle } from "./ui/card";
@@ -15,6 +16,7 @@ import {
 import { FaBath, FaBed, FaHeart, FaRegHeart, FaShare } from "react-icons/fa6";
 import { toast } from "sonner";
 import { formatDate } from "@/lib/utils";
+import { DotFilledIcon, DotIcon } from "@radix-ui/react-icons";
 
 export type AdListingProps = {
   ad: AdModel;
@@ -23,8 +25,28 @@ export type AdListingProps = {
 export function AdListing(props: AdListingProps) {
   const { ad } = props;
 
-  const [demo, setDemo] = useState(false);
+  const [isFav, setIsFav] = useState(false);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [api, setApi] = useState<CarouselApi>();
+
   const navigate = useNavigate();
+
+  const images: string[] = [
+    "https://placehold.co/400x160?text=Image 1",
+    "https://placehold.co/400x160?text=Image 2",
+    "https://placehold.co/400x160?text=Image 3",
+  ];
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setSlideIndex(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setSlideIndex(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   return (
     <div
@@ -37,33 +59,43 @@ export function AdListing(props: AdListingProps) {
         <div className="text-xs absolute top-2 right-2 bg-black/20 text-white rounded p-1 z-50">
           {formatDate(ad.createdAt || "")}
         </div>
-        <CardTitle>
-          <Carousel>
+        <CardTitle className="relative">
+          <Carousel setApi={setApi}>
             <CarouselContent>
-              <CarouselItem>
-                <img
-                  className="w-full h-full object-cover"
-                  src="https://placehold.co/400x160?text=Image 1"
-                  alt="ad"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                <img
-                  className="w-full h-full object-cover"
-                  src="https://placehold.co/400x160?text=Image 2"
-                  alt="ad"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                <img
-                  className="w-full h-full object-cover"
-                  src="https://placehold.co/400x160?text=Image 3"
-                  alt="ad"
-                />
-              </CarouselItem>
+              {images.map((image, index) => (
+                <CarouselItem key={index}>
+                  <img
+                    className="w-full h-full object-cover"
+                    src={image}
+                    alt="ad"
+                  />
+                </CarouselItem>
+              ))}
             </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
+            <div className="absolute z-50 bottom-1 left-0 right-0 flex justify-center gap-2">
+              {images.map((_, index) => {
+                if (index === slideIndex - 1) {
+                  return (
+                    <DotFilledIcon
+                      className="w-5 h-5"
+                      onClick={(e) => e.stopPropagation()}
+                      key={index}
+                    />
+                  );
+                }
+
+                return (
+                  <DotIcon
+                    className="w-5 h-5"
+                    key={index}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      api?.scrollTo(index);
+                    }}
+                  />
+                );
+              })}
+            </div>
           </Carousel>
         </CardTitle>
         <CardContent className="p-2 relative">
@@ -83,21 +115,21 @@ export function AdListing(props: AdListingProps) {
             <span>{ad.address}</span>
           </div>
           <div className="absolute flex flex-col gap-2 top-2 right-2">
-            {demo && (
+            {isFav && (
               <FaHeart
                 fill="red"
                 onClick={(e) => {
                   e.stopPropagation();
-                  setDemo(false);
+                  setIsFav(false);
                 }}
               />
             )}
-            {!demo && (
+            {!isFav && (
               <FaRegHeart
                 className="hover:fill-red-500"
                 onClick={(e) => {
                   e.stopPropagation();
-                  setDemo(true);
+                  setIsFav(true);
                 }}
               />
             )}
