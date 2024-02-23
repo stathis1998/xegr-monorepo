c2e26c8cd637e352cde81441ac73fed679b13a9c
= <=>
2024-02-23 15:00:03 +0200

    Fixed width and added white bg

--- packages/react-frontend/src/pages/secured/adsView.tsx
+++ packages/react-frontend/src/pages/secured/adsView.tsx
@@ -64,92 +64,95 @@ export function AdsView(props: AdsViewProps) {
   ];
 
   return (
-    <div className="p-4 h-full">
-      <Container className="py-4 flex flex-col h-full">
-        <section>
-          <h2 className="font-bold">Find Your Perfect Home</h2>
-          <p className="text-black/70">
-            Search through our extensive list of properties to find the perfect
-            home for you.
-          </p>
-        </section>
+    <div className="h-full">
+      <Container className="py-4 flex flex-col h-full" fluid>
+        <Container className="p-4 flex-grow overscroll-auto h-0">
+          <section>
+            <h2 className="font-bold">Find Your Perfect Home</h2>
+            <p className="text-black/70">
+              Search through our extensive list of properties to find the
+              perfect home for you.
+            </p>
+          </section>
 
-        <Separator className="bg-gray-300 my-4" />
+          <Separator className="bg-gray-300 my-4" />
 
-        <div className="flex flex-col gap-2 md:flex-row-reverse md:justify-between items-center">
-          <div className="flex gap-2">
-            <Button>Create Listing</Button>
+          <div className="flex flex-col gap-2 md:flex-row-reverse md:justify-between items-center">
+            <div className="flex gap-2">
+              <Button>Create Listing</Button>
+            </div>
+            <div className="flex gap-2">
+              <FilterButton
+                label="Price"
+                subLabel1="From"
+                subLabel2="To"
+                onChange={(value) => console.log(value)}
+              />
+              <FilterButton
+                label="Area"
+                subLabel1="From"
+                subLabel2="To"
+                onChange={(value) => console.log(value)}
+              />
+              <FilterButton
+                label="Bedrooms"
+                subLabel1="From"
+                subLabel2="To"
+                onChange={(value) => console.log(value)}
+              />
+              <FilterButton
+                label="Bathrooms"
+                subLabel1="From"
+                subLabel2="To"
+                onChange={(value) => console.log(value)}
+              />
+            </div>
           </div>
-          <div className="flex gap-2">
-            <FilterButton
-              label="Price"
-              subLabel1="From"
-              subLabel2="To"
-              onChange={(value) => console.log(value)}
-            />
-            <FilterButton
-              label="Area"
-              subLabel1="From"
-              subLabel2="To"
-              onChange={(value) => console.log(value)}
-            />
-            <FilterButton
-              label="Bedrooms"
-              subLabel1="From"
-              subLabel2="To"
-              onChange={(value) => console.log(value)}
-            />
-            <FilterButton
-              label="Bathrooms"
-              subLabel1="From"
-              subLabel2="To"
-              onChange={(value) => console.log(value)}
-            />
+
+          <Separator className="bg-gray-300 my-4" />
+
+          <section className="flex-grow h-0 overflow-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+            <EmptyArea />
+            {Array.from({ length: 20 }).map((_, index) => (
+              <AdListing key={index} ad={testAds[index % 3]} />
+            ))}
+          </section>
+
+          <Separator className="bg-gray-300 my-4" />
+        </Container>
+        <Container fluid className="my-2 px-4 py-8 bg-white">
+          <div className="flex flex-col justify-center items-center">
+            <Button
+              className="w-full max-w-64 mx-auto"
+              onClick={() => {
+                const luckyAd =
+                  testAds[Math.floor(Math.random() * testAds.length)];
+                if (luckyAd) {
+                  navigate(`/ads/${luckyAd.id}`);
+                  window.scrollTo(0, 0);
+
+                  toast.success(
+                    "Congrats! You've hit the jackpot of homes! This one's got walls, a roof, and even windows. What are the odds?",
+                    { duration: 5000 }
+                  );
+                } else {
+                  toast.message(
+                    "Looks like the housing market is playing hide and seek! No homes found - maybe they're all out getting yard work done?",
+                    { duration: 5000 }
+                  );
+                }
+              }}
+            >
+              I'm Feeling Lucky
+            </Button>
+            <p className="text-black/70 text-center text-sm w-full mt-2 max-w-[500px]">
+              <span className="font-bold">Disclaimer:</span>{" "}
+              <span className="italic">'I'm Feeling Lucky'</span> may lead to
+              spontaneous home discoveries. Side effects include laughter and
+              sudden urges to redecorate. Click responsibly!
+            </p>
           </div>
-        </div>
-
-        <Separator className="bg-gray-300 my-4" />
-
-        <section className="flex-grow h-0 overflow-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-          <EmptyArea />
-          {Array.from({ length: 20 }).map((_, index) => (
-            <AdListing key={index} ad={testAds[index % 3]} />
-          ))}
-        </section>
-
-        <Separator className="bg-gray-300 my-4" />
-
-        <div className="flex flex-col justify-center items-center">
-          <Button
-            className="w-full max-w-64 mx-auto"
-            onClick={() => {
-              const luckyAd =
-                testAds[Math.floor(Math.random() * testAds.length)];
-              if (luckyAd) {
-                navigate(`/ads/${luckyAd.id}`);
-                window.scrollTo(0, 0);
-
-                toast.success(
-                  "Congrats! You've hit the jackpot of homes! This one's got walls, a roof, and even windows. What are the odds?",
-                  { duration: 5000 }
-                );
-              } else {
-                toast.message(
-                  "Looks like the housing market is playing hide and seek! No homes found - maybe they're all out getting yard work done?",
-                  { duration: 5000 }
-                );
-              }
-            }}
-          >
-            I'm Feeling Lucky
-          </Button>
-          <p className="text-black/70 text-center text-sm w-full mt-2 max-w-[350px]">
-            <span className="font-bold">Disclaimer:</span>{" "}
-            <span className="italic">'I'm Feeling Lucky'</span> may lead to
-            spontaneous home discoveries. Side effects include laughter and
-            sudden urges to redecorate. Click responsibly!
-          </p>
-        </div>
+        </Container>
       </Container>
     </div>
   );
