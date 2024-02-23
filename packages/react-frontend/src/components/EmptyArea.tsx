f724969e9b2c5b05742cc7c349c72394b1c62f33
= <=>
2024-02-23 14:33:13 +0200

    Added some animation to the empty area

--- packages/react-frontend/src/components/EmptyArea.tsx
+++ packages/react-frontend/src/components/EmptyArea.tsx
@@ -2,8 +2,8 @@ import { FaHouse, FaPlus } from "react-icons/fa6";
 
 export function EmptyArea() {
   return (
-    <div className="w-full h-full bg-gray-200 rounded border-2 border-dashed border-black/30 cursor-pointer flex justify-center items-center p-4">
-      <div className="space-y-2">
+    <div className="w-full h-full bg-gray-200 rounded border-2 border-dashed border-black/30 cursor-pointer flex justify-center items-center p-4 hover:scale-105 active:scale-100 transition-all">
+      <div className="space-y-2 select-none">
         <div className="relative">
           <FaHouse className="w-20 h-20 mx-auto text-black/30 animate-pulse" />
           <FaPlus className="w-10 h-10 absolute -top-2 right-8 text-black/30 animate-pulse" />
